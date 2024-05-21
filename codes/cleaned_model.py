22500c557ef8e6f1bf42ff5d33dfdd3743ce29cf
Jesse Cha <gotenkscha@gmail.com>
2024-05-20 19:35:09 -0700

    Update cleaned_model.py

--- codes/cleaned_model.py
+++ codes/cleaned_model.py
@@ -5,25 +5,6 @@ from tensorflow.keras.layers import Input, Flatten, Dense, Dropout, LayerNormali
 from tensorflow.keras import layers, optimizers, regularizers
 from tensorflow.keras.losses import mean_squared_error
 import tensorflow as tf
-import tensorflow_probability as tfp
-tfd = tfp.distributions
-
-def total_steering_loss(y_true, y_pred):
-    n = tf.cast(tf.shape(y_true), dtype=tf.float32)
-    f = lambda x: tfd.Independent(tfd.Cauchy(loc=x, scale=1.))
-    kde_true = tfd.MixtureSameFamily(
-        mixture_distribution=tfd.Categorical(probs=[1 / n] * n),
-        components_distribution=f(y_true)
-    )
-    kde_pred = tfd.MixtureSameFamily(
-        reparameterize=False,
-        mixture_distribution=tfd.Categorical(probs=[1 / n] * n),
-        components_distribution=f(y_pred)
-    )
-    mean1 = kde_true.mean()
-    mean2 = kde_pred.mean()
-    steering_loss = tf.reduce_mean((mean1 - mean2) ** 2)
-    return steering_loss
 
 class Patches(layers.Layer):
     def __init__(self, patch_size):
@@ -76,42 +57,8 @@ def mlp(x, hidden_units, dropout_rate):
         x = layers.Dense(units, activation=tf.nn.gelu)(x)
         x = layers.Dropout(dropout_rate)(x)
     return x
-
-def build_transformer_itdm(w, h, d, s, s_start, s_end, batch_size):
-    patch_size = w
-    num_patches = 28
-    projection_dim = num_patches
-    num_heads = 5
-    transformer_units = [projection_dim * 2, projection_dim]
-    transformer_layers = 2
-    mlp_head_units = [64, 64]
-
-    image_inputs = Input(shape=(h, w, d), name='image_input')
-    patches = Patches(patch_size)(image_inputs)
-    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
-
-    for _ in range(transformer_layers):
-        x1 = LayerNormalization(epsilon=1e-6)(encoded_patches)
-        attention_output = MultiHeadAttention(num_heads=num_heads, key_dim=projection_dim, dropout=0.1)(x1, x1)
-        x2 = Add()([attention_output, encoded_patches])
-        x3 = LayerNormalization(epsilon=1e-6)(x2)
-        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
-        encoded_patches = Add()([x3, x2])
-
-    representation = LayerNormalization(epsilon=1e-6)(encoded_patches)
-    representation = Flatten()(representation)
-    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.1)
-    angle_out = Dense(1, activation='linear', name='angle_out')(features)
-
-    model = Model(inputs=[image_inputs], outputs=[angle_out])
-    optimizer = optimizers.Adam(lr=0.05)
-    model.compile(loss={'angle_out': total_steering_loss},
-                  optimizer=optimizer,
-                  metrics={'angle_out': [total_steering_loss, 'mse']}, loss_weights=[1.0])
-    model.summary()
-    return model
-
-def build_transformer3d(w, h, d, s):
+    
+def build_racevit(w, h, d, s):
     patch_size = 32
     num_patches = 32
     projection_dim = num_patches
@@ -139,40 +86,6 @@ def build_transformer3d(w, h, d, s):
 
     model = Model(inputs=[image_inputs], outputs=[angle_out])
     optimizer = optimizers.Adam(lr=0.05)
-    model.compile(loss={'angle_out': mean_squared_error},
-                  optimizer=optimizer,
-                  metrics={'angle_out': ['mse']}, loss_weights=[1.0])
-    model.summary()
-    return model
-
-def build_transformer(w, h, d, s):
-    patch_size = w
-    num_patches = w
-    projection_dim = num_patches
-    num_heads = 5
-    transformer_units = [projection_dim * 2, projection_dim]
-    transformer_layers = 3
-    mlp_head_units = [128, 128]
-
-    image_inputs = Input(shape=(h, w, d), name='image_input')
-    patches = Patches(patch_size)(image_inputs)
-    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
-
-    for _ in range(transformer_layers):
-        x1 = LayerNormalization(epsilon=1e-6)(encoded_patches)
-        attention_output = MultiHeadAttention(num_heads=num_heads, key_dim=projection_dim, dropout=0.1)(x1, x1)
-        x2 = Add()([attention_output, encoded_patches])
-        x3 = LayerNormalization(epsilon=1e-6)(x2)
-        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
-        encoded_patches = Add()([x3, x2])
-
-    representation = LayerNormalization(epsilon=1e-6)(encoded_patches)
-    representation = Flatten()(representation)
-    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.1)
-    angle_out = Dense(1, activation='linear', name='angle_out')(features)
-
-    model = Model(inputs=[image_inputs], outputs=[angle_out])
-    optimizer = optimizers.Adam(lr=0.01)
     model.compile(loss={'angle_out': mean_squared_error},
                   optimizer=optimizer,
                   metrics={'angle_out': ['mse']}, loss_weights=[1.0])
